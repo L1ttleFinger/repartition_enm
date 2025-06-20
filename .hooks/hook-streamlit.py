7b3e67358c127c2ab23aa0ea65d6bf4756aa41e4
Vincent O'Luasa <vincent.oluasa@gmail.com>
2025-06-20 14:50:24 +0200

    Test

--- .hooks/hook-streamlit.py
+++ .hooks/hook-streamlit.py
@@ -1,3 +1,5 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, copy_metadata
 
-datas = collect_data_files("streamlit", include_py_files=True)
+datas = [("enm_env/lib/python3.9/site-packages/streamlit/runtime", "./streamlit/runtime"), ("enm_env/lib/python3.9/site-packages/streamlit/static", "./streamlit/static")]
+datas += collect_data_files("streamlit")
+datas += copy_metadata("streamlit")
