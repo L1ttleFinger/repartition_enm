d6aa52364f45bfc5736504d54e81aabd448aa155
Vincent O'Luasa <vincent.oluasa@gmail.com>
2025-06-20 10:35:22 +0200

    Fix app launching issue

--- repartition_enm.py
+++ repartition_enm.py
@@ -1,13 +1,30 @@
-# launcher.py
-import subprocess
-import sys
 import os
+import sys
+import subprocess
+import webbrowser
+import threading
+import time
 
 def resource_path(relative_path):
     """Get absolute path to resource (handles PyInstaller bundle or dev)."""
     base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
     return os.path.join(base_path, relative_path)
 
+def open_browser():
+    """Delay opening the browser to wait for Streamlit server to start"""
+    time.sleep(2)
+    webbrowser.open_new("http://localhost:8501")
+
 if __name__ == "__main__":
     app_path = resource_path("app/app.py")
-    subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
+
+    print("Opening browser")
+    threading.Thread(target=open_browser).start()
+
+    print("Launching streamlit app")
+    subprocess.run([
+        sys.executable, "-m", "streamlit", "run", app_path,
+        "--server.headless=true",
+        "--browser.gatherUsageStats=false",
+        "--server.runOnSave=false"
+    ])
