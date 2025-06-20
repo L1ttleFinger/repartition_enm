68d33645e71a9ab14ddd45ea9ab1d3e3ce0f7230
Vincent O'Luasa <vincent.oluasa@gmail.com>
2025-06-20 10:02:57 +0200

    Fix parameters path

---
+++ repartition_enm.py
@@ -0,0 +1,14 @@
+# launcher.py
+import subprocess
+import sys
+import os
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+if __name__ == "__main__":
+    app_path = resource_path("app/app.py")
+    subprocess.run(["streamlit", "run", app_path])
