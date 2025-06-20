51e6f29463c7ab2c020616352ebf7a1b88b113ad
Vincent O'Luasa <vincent.oluasa@gmail.com>
2025-06-20 10:54:35 +0200

    Fix app launching issue

--- repartition_enm.py
+++ repartition_enm.py
@@ -10,24 +10,24 @@ def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
     return os.path.join(base_path, relative_path)
 
-def open_browser():
-    print("Calling open_browser)")
-    """Delay opening the browser to wait for Streamlit server to start"""
-    time.sleep(2)
-    webbrowser.open_new("http://localhost:8501")
+# def open_browser():
+#     print("Calling open_browser)")
+#     """Delay opening the browser to wait for Streamlit server to start"""
+#     time.sleep(2)
+#     webbrowser.open_new("http://localhost:8501")
 
 if __name__ == "__main__":
     app_path = resource_path("app/app.py")
 
-    print("Opening browser")
-    threading.Thread(target=open_browser).start()
+    # print("Opening browser")
+    # threading.Thread(target=open_browser).start()
 
     print("Launching streamlit app")
     subprocess.run([
         sys.executable, "-m", "streamlit", "run", app_path,
-        "--server.headless=true",
-        "--browser.gatherUsageStats=false",
-        "--browser.serverAddress=localhost",
-        "--server.runOnSave=false",
-        "--browser.serverPort=8501"
+        # "--server
+        # "--browser.gatherUsageStats=false",
+        # "--browser.serverAddress=localhost",
+        # "--server.runOnSave=false",
+        # "--browser.serverPort=8501"
     ])
